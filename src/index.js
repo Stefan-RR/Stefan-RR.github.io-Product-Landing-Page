c6fddf9d9edd4a061b871589e1dbfc6bb59c121f
Stefan-RR <stefan.ruchlewicz@outlook.com>
2022-12-03 13:20:57 -0700

    Added menu disappear on page click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 function dropDown() {
     document.getElementById('dropDown').classList.toggle('show');
 }
-
+window.onclick = function(ev){
+    if (ev.target.className !== 'navButton'){
+        document.getElementById('dropDown').classList.remove('show');
+    }
+}
