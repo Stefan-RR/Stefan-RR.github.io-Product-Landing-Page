78ce8f50e09b3ec88d7a150dfc07574f814eb15c
Stefan-RR <stefan.ruchlewicz@outlook.com>
2023-01-05 19:30:24 -0700

    Changed nav menu from drop down to slide out

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 function dropDown() {
     document.getElementById('dropDown').classList.toggle('show');
 }
+function preventScroll() {
+    document.getElementById('bodyDiv').classList.toggle('no-scroll');
+}
 window.onclick = function(ev){
-    if (ev.target.className !== 'navButton'){
+    if (ev.target.className !== 'button-box'){
+        document.getElementById('bodyDiv').classList.remove('no-scroll');
         document.getElementById('dropDown').classList.remove('show');
     }
 }
